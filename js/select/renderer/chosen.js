e4d9746adea0f4465a41128facc93576238301ca
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2016-10-19 14:18:54 +0100

    Provide a specific renderer for chosen

---
+++ js/select/renderer/chosen.js
@@ -0,0 +1,25 @@
+'use strict';
+var $ = require('jquery');
+
+var ChosenRenderer = {
+    render: function ($container, header, data) {
+        this.populate(data);
+        this.showFilter(this.$dom, $container, header, data);
+        this.$dom.chosen(this.rendererOptions || {});
+
+        return this;
+    },
+
+    showFilter: function($dom, $container, header, data) {
+        $container.append(this.$dom);
+    },
+
+    refresh: function (data) {
+        this.update(data);
+        this.$dom.trigger('chosen:updated');
+
+        return this;
+    }
+};
+
+module.exports = ChosenRenderer;
